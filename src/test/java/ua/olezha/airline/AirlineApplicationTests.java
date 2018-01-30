962c3a9c5953417996831796d840fbbf805da315
Oleh Shklyar <pio_ua@ukr.net>
2018-01-30 22:10:44 +0200

    todo tests

--- src/test/java/ua/olezha/airline/AirlineApplicationTests.java
+++ src/test/java/ua/olezha/airline/AirlineApplicationTests.java
@@ -1,6 +1,7 @@
 package ua.olezha.airline;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,32 @@ public class AirlineApplicationTests implements ApplicationRunner {
         assertThat(shell.evaluate(() -> "add Commuterliner")).isNull();
         Assert.assertThat(((List<Aircraft>) shell.evaluate(() -> "show")).size() - firstAircraftListSize,
                 is(1));
+    }
+
+    @Ignore
+    @Test
+    public void deleteAllSuccessfully() {
+        shell.evaluate(() -> "delete -all");
+        Assert.assertThat(((List) shell.evaluate(() -> "show")).size(),
+                is(0));
+    }
+
+    @Ignore
+    @Test
+    public void searchByAnyParametersSuccessfully() {
+        shell.evaluate(() -> "delete -all");
+
+        shell.evaluate(() -> "add Commuterliner 0 1 2 3");
+        shell.evaluate(() -> "add Helicopter 1 2 3 4");
+        shell.evaluate(() -> "add WideBodyAirliner 2 3 4 5");
+        shell.evaluate(() -> "add Commuterliner 3 4 5 6");
+        shell.evaluate(() -> "add Helicopter 4 5 6 7");
+        shell.evaluate(() -> "add WideBodyAirliner 5 6 7 8");
+
+        assertThat(((List) shell.evaluate(() -> "search -seating-capacity 4")).size()).isEqualTo(1);
+        assertThat(((List) shell.evaluate(() -> "search -carrying-capacity-kg 8")).size()).isEqualTo(1);
+        assertThat(((List) shell.evaluate(() -> "search -flight-range-km 5")).size()).isEqualTo(1);
+        assertThat(((List) shell.evaluate(() -> "search -fuel-consumption-liters-per-hour 3")).size()).isEqualTo(1);
     }
 }
 
