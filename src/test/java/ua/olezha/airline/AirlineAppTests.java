cb2909c0237e3dca406f55c2acef357eef938837
Oleh <pio_ua@ukr.net>
2018-02-01 19:12:22 +0200

    1

---
+++ src/test/java/ua/olezha/airline/AirlineAppTests.java
@@ -0,0 +1,13 @@
+package ua.olezha.airline;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class AirlineAppTests {
+
+    @Test
+    public void airlineApplicationTest() throws IOException {
+        AirlineApplication.main(new String[]{});
+    }
+}
