70829c5162f73cdc2febc66457538c41df6a1ca8
Oleh Shklyar <pio_ua@ukr.net>
2018-02-04 18:20:56 +0200

    CurrentSessionContext configured Exception

--- src/main/java/ua/olezha/airline/repository/AircraftRepositoryImpl.java
+++ src/main/java/ua/olezha/airline/repository/AircraftRepositoryImpl.java
@@ -3,12 +3,21 @@ package ua.olezha.airline.repository;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ua.olezha.airline.model.aircraft.Aircraft;
 
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
+@Repository
 public class AircraftRepositoryImpl implements BetweenExamplesRepository<Aircraft> {
+
+    /**
+     * org.springframework.orm.jpa.JpaSystemException:
+     *     No CurrentSessionContext configured!;
+     * nested exception is org.hibernate.HibernateException:
+     *     No CurrentSessionContext configured!
+     */
 
     private final SessionFactory sessionFactory;
 
